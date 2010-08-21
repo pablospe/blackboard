c10f0962a012e06d9266f70d04028fd51ce38cb0
spe <spe@d7e620e4-ace5-11df-a369-b9537fdb6e25>
2010-08-21 17:44:00 +0000

    Reconoce sólo: "a", "b" y "c" (de un solo trazo) Comienza un nuevo trazo cada vez que empiezo a escribir.
    git-svn-id: svn://neker.dyndns.org/projects/blackboard@3 d7e620e4-ace5-11df-a369-b9537fdb6e25

--- blackboard.cpp
+++ blackboard.cpp
@@ -2,11 +2,19 @@
 #include <opencv/highgui.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
 IplImage* img0 = 0, *img = 0;
 CvPoint prev_pt = {-1,-1};
+
+vector<Point> contour_a;
+vector<Point> contour_b;
+vector<Point> contour_c;
+
+vector<Point> current_contour;
 
 void on_mouse( int event, int x, int y, int flags, void* )
 {
@@ -16,21 +24,47 @@ void on_mouse( int event, int x, int y, int flags, void* )
     if( event == CV_EVENT_LBUTTONUP || !(flags & CV_EVENT_FLAG_LBUTTON) ) {
         if( prev_pt.x > 0 ) {
             printf( "End!\n");
+
+            float a, b, c;
+
+            a = matchShapes( Mat(contour_a), Mat(current_contour), CV_CONTOURS_MATCH_I3, 0);
+            b = matchShapes( Mat(contour_b), Mat(current_contour), CV_CONTOURS_MATCH_I3, 0);
+            c = matchShapes( Mat(contour_c), Mat(current_contour), CV_CONTOURS_MATCH_I3, 0);
+
+            cout << "matchShapes(a,.) = " << a << endl;
+            cout << "matchShapes(b,.) = " << b << endl;
+            cout << "matchShapes(c,.) = " << c << endl;
+
+            if( a < b ) {
+                if( a < c )
+                    cout << "Letra a!!!!!\n";
+                else
+                    cout << "Letra c!!!!!\n";
+            }
+            else {
+                if( b < c )
+                    cout << "Letra b!!!!!\n";
+                else
+                    cout << "Letra c!!!!!\n";
+            }
         }
         prev_pt = cvPoint(-1,-1);
     }
     else if( event == CV_EVENT_MOUSEMOVE && (flags & CV_EVENT_FLAG_LBUTTON) )
     {
         CvPoint pt = cvPoint(x,y);
-        printf( "Point: (%d,%d)\n", x, y );
 
         if( prev_pt.x < 0 ) {
             prev_pt = pt;
             printf( "Start moving!\n");
+            current_contour.clear();
         }
         else {
             cvLine( img, prev_pt, pt, cvScalarAll(255), 5, 8, 0 );
         }
+
+        printf( "Point: (%d,%d)\n", x, y );
+        current_contour.push_back(Point2f(x,y));
 
         prev_pt = pt;
         imshow( "image", img );
@@ -40,11 +74,16 @@ void on_mouse( int event, int x, int y, int flags, void* )
 
 int main( int argc, char** argv )
 {
+    #include "train/contour_a.cpp"
+    #include "train/contour_b.cpp"
+    #include "train/contour_c.cpp"
+
+
     printf( "Hot keys: \n"
             "\tESC - quit the program\n"
             "\tr - restore the original image\n" );
 
-    namedWindow( "image", 1 );
+    namedWindow( "image" );
     cvSetMouseCallback( "image", on_mouse, 0 );
 
     /* create an image */
