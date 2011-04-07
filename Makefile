2f6b7fee1a7b2a59f6c23085ce8cfa308b1aeb97
spe <spe@d7e620e4-ace5-11df-a369-b9537fdb6e25>
2011-04-07 02:24:55 +0000

    First version using lipitk: make && ./blackboard NUMERALS_NUM
    git-svn-id: svn://neker.dyndns.org/projects/blackboard@17 d7e620e4-ace5-11df-a369-b9537fdb6e25

--- Makefile
+++ Makefile
@@ -1,7 +1,34 @@
-all: blackboard
+include $(LIPI_ROOT)/global.mk
 
-CFLAGS += `pkg-config --libs --cflags opencv` -g
-CXXFLAGS += `pkg-config --libs --cflags opencv` -g
+SRC=blackboard.cpp
+
+H=$(SRC:.cpp=.h)
+
+OBJ=$(SRC:.cpp=.o)
+
+TARGET=blackboard
+
+INC= -I$(LIPITK_SRC_UTILS_LIB) \
+     -I$(LIPITK_SRC_INCLUDE)
+
+LIB= -lutil \
+     -lcommon \
+     -lshaperecommon
+
+LIBDIR= $(LIPITK_STATIC_LIBDIR)
+
+CFLAGS   += `pkg-config --cflags opencv` -g
+CPPFLAGS += `pkg-config --cflags opencv` -g
+# CPPFLAGS += -W -Wall
+LINKLIB  += `pkg-config --libs opencv`
+
+
+all: ${OBJ} ${SRC}
+		$(CC) $(CPPFLAGS) -o ${TARGET} ${OBJ} $(LINKLIB) $(LIB) -L${LIBDIR}
 
 clean:
-	rm blackboard
+		-@$(REMOVE) -fv ${OBJ}
+		-@$(REMOVE) -fv ${TARGET}
+
+.cpp.o:${SRC}
+		$(CC) ${INC} $(CPPFLAGS) -c $< -o $@
--- blackboard.cpp
+++ blackboard.cpp
@@ -5,6 +5,48 @@
 #include <stdlib.h>
 #include <iostream>
 
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <dlfcn.h>
+#endif
+
+#pragma warning(disable:4786)
+
+#include <string>
+#include "LTKInkFileReader.h"
+#include "LTKLipiEngineInterface.h"
+#include "LTKMacros.h"
+#include "LTKInc.h"
+#include "LTKTypes.h"
+#include "LTKTrace.h"
+
+#ifndef _WIN32
+#define MAX_PATH 1024
+#endif
+
+/* function pointer declaration to get the function address of "createLTKLipiEngine" */
+typedef LTKLipiEngineInterface* (*FN_PTR_CREATELTKLIPIENGINE) (void);
+FN_PTR_CREATELTKLIPIENGINE createLTKLipiEngine;
+
+/* Pointer to the LipiEngine interface */
+LTKLipiEngineInterface *ptrObj = NULL;
+
+void *hLipiEngine;
+int MapFunctions();
+
+#include "LTKLoggerUtil.h"
+#include "LTKErrors.h"
+#include "LTKOSUtilFactory.h"
+#include "LTKOSUtil.h"
+
+char strLogFile[MAX_PATH] = "shaperectst.log";
+string strLogFileName;
+LTKOSUtil* utilPtr = LTKOSUtilFactory::getInstance();
+
+LTKShapeRecognizer *pShapeReco = NULL;
+
+
 using namespace cv;
 using namespace std;
 
@@ -13,7 +55,7 @@ typedef vector<Point> Contour;
 IplImage* img0 = 0, *img = 0;
 CvPoint prev_pt = {-1,-1};
 
-int hist_size[] = {128, 128};
+int hist_size[] = {32, 32};
 
 map<string, Contour>       contours;
 map<string, CvHistogram*>  histograms;
@@ -27,6 +69,9 @@ Contour contour_e;
 
 Contour current_contour;
 
+int shaperectst_init(int argc, char** argv);
+int shaperectst_recog(LTKTraceGroup &inTraceGroup);
+int shaperectst_end();
 
 void copyFrom(Seq<Point> &seq, Contour& vec)
 {
@@ -108,20 +153,20 @@ void on_mouse ( int event, int x, int y, int flags, void* )
 
     if( event == CV_EVENT_LBUTTONUP || ! ( flags & CV_EVENT_FLAG_LBUTTON ) ) {
         if( prev_pt.x > 0 ) {
-            printf( "End!\n" );
+            cout << "End!\n";
 
             CvHistogram *current_hist = cvCreateHist( 2, hist_size, CV_HIST_ARRAY );
             calcPGH( current_contour, current_hist );
 
-            map< string, Contour >::iterator it;
-            for( it = contours.begin() ; it != contours.end(); it++ ) {
-                const string &letter  = (*it).first;
-                Contour      &contour = (*it).second;
+//             map< string, Contour >::iterator it;
+//             for( it = contours.begin() ; it != contours.end(); it++ ) {
+//                 const string &letter  = (*it).first;
+//                 Contour      &contour = (*it).second;
 
                 // Metodo 1 - cvCompareHist
-                float res = cvCompareHist( histograms[letter], current_hist, CV_COMP_CORREL );
-                cout << "cvCompareHist(" << letter << ",.) = " << res << endl;
-                res = -res; // high score represents a better match
+//                 float res = cvCompareHist( histograms[letter], current_hist, CV_COMP_CORREL );
+//                 cout << "cvCompareHist(" << letter << ",.) = " << res << endl;
+//                 res = -res; // high score represents a better match
 
                 // Metodo 2 - matchShapes
 //                 float res = matchShapes( Mat(contour), Mat(current_contour), CV_CONTOURS_MATCH_I3, 0);
@@ -131,10 +176,26 @@ void on_mouse ( int event, int x, int y, int flags, void* )
 //                 float res = calcEMD2( histograms[letter], current_hist );
 //                 cout << "cvCalcEMD2(" << letter << ",.) = " << res << endl;
 
-                result[ res ] = letter;
-            }
+                // Metodo 4 - Lipi
+                float res = 0;
+                LTKTrace trace;
+                for( vector< Point >::iterator iter = current_contour.begin();
+                     iter != current_contour.end(); iter++)
+                {
+                    floatVector point;
+                    
+                    point.push_back( iter->x );
+                    point.push_back( iter->y );
+                    
+                    trace.addPoint(point);
+                }
+                LTKTraceGroup traceGroup(trace);
+                res = shaperectst_recog(traceGroup);
 
-            cout << "Letra " << result.begin()->second  << "!!!!!\n";
+//                 result[ res ] = letter;
+//             }
+
+//             cout << "Letra " << result.begin()->second  << "!!!!!\n";
 //             cout << "Letra " << result.rbegin()->second << "!!!!!\n"; // last one - rbeing (reverse begin)
 
             current_contour.clear();
@@ -159,7 +220,7 @@ void on_mouse ( int event, int x, int y, int flags, void* )
             current_contour.push_back ( Point2f ( x, y ) );
 
             prev_pt = pt;
-            imshow( "image", img );
+            imshow( "Blackboard", img );
         }
 }
 
@@ -178,29 +239,25 @@ int main( int argc, char** argv )
     contours[ "d" ] = contour_d;
     contours[ "e" ] = contour_e;
 
-    // Calculate histograms (for training letters)
-    map<string, Contour>::iterator it;
-    for( it = contours.begin() ; it != contours.end(); it++ ) {
-        const string  &letter  = ( *it ).first;
-        Contour       &contour = ( *it ).second;
 
-        histograms[letter] = cvCreateHist( 2, hist_size, CV_HIST_ARRAY );;
-        calcPGH( contour, histograms[letter] );
+    if( shaperectst_init(argc, argv) != 0 )
+    {
+        cout << "Saliendo...\n";
+        return -1;
     }
-
-
-    printf( "Hot keys: \n"
+    
+    cout << "Hot keys: \n"
             "\tESC - quit the program\n"
-            "\tr - restore the blackboard\n" );
+            "\tr - restore the blackboard\n";
 
-    namedWindow( "image" );
-    cvSetMouseCallback( "image", on_mouse, 0 );
+    namedWindow( "Blackboard" );
+    cvSetMouseCallback( "Blackboard", on_mouse, 0 );
 
-    /* create an image */
-    IplImage *img0 = cvCreateImage( cvSize( 400, 400 ), IPL_DEPTH_8U, 3 );
+    /* create an Blackboard */
+    IplImage *img0 = cvCreateImage( cvSize( 800, 600 ), IPL_DEPTH_8U, 3 );
 
     img = cvCloneImage( img0 );
-    imshow( "image", img );
+    imshow( "Blackboard", img );
 
     for( ;; ) {
         int c = waitKey( 0 );
@@ -210,15 +267,251 @@ int main( int argc, char** argv )
 
         if(( char ) c == 'r' ) {
             cvCopy( img0, img );
-            cvShowImage( "image", img );
+            cvShowImage( "Blackboard", img );
         }
     }
 
 
     // Release Histograms
-    for( it = contours.begin() ; it != contours.end(); it++ ) {
+    for( map< string, Contour >::const_iterator it = contours.begin() ; it != contours.end(); it++ ) {
         cvReleaseHist( &histograms[( *it ).first] );
     }
 
+    shaperectst_end();
+
     return 1;
+}
+
+
+
+
+
+
+// Es el main del programa shaperectst
+int shaperectst_init(int argc, char** argv)
+{
+    char *envstring = NULL;
+    int iResult;
+
+
+    // first argument is the logical project name and the
+    // second argument is the ink file to recognize
+    if(argc < 3)
+    {
+        cout << endl << "Usage:";
+        cout << endl << "shaperectst <logical projectname> <ink file to recognize>";
+        cout << endl;
+//         delete utilPtr;
+//         return -1;
+    }
+
+    //Get the LIPI_ROOT environment variable
+    envstring = getenv(LIPIROOT_ENV_STRING);
+    if(envstring == NULL)
+    {
+        cout << endl << "Error, Environment variable is not set LIPI_ROOT" << endl;
+        delete utilPtr;
+        return -1;
+    }
+
+    //Load the LipiEngine.DLL
+    hLipiEngine = NULL;
+    iResult = utilPtr->loadSharedLib(envstring, LIPIENGINE_MODULE_STR, &hLipiEngine);
+
+    if(iResult != SUCCESS)
+    {
+        cout << "Error loading LipiEngine module" << endl;
+        delete utilPtr;
+        return -1;
+    }
+
+
+    if(MapFunctions() != 0)
+    {
+        cout << "Error fetching exported functions of the module" << endl;
+        delete utilPtr;
+        return -1;
+    }
+
+    LTKInkFileReader kkkkkk;  // !!! Problema de linkeo, si no uso esta linea
+
+    //create an instance of LipiEngine Module
+    ptrObj = createLTKLipiEngine();
+
+    // set the LIPI_ROOT path in Lipiengine module instance
+    ptrObj->setLipiRootPath(envstring);
+
+    //Initialize the LipiEngine module
+    iResult = ptrObj->initializeLipiEngine();
+    if(iResult != SUCCESS)
+    {
+        cout << iResult <<": Error initializing LipiEngine." << endl;
+
+        utilPtr->unloadSharedLib(hLipiEngine);
+        delete utilPtr;
+
+        return -1;
+    }
+
+    //Assign the logical name of the project to this string, i.e. TAMIL_CHAR
+    //(or) "HINDI_GESTURES"
+    string strLogicalName = string(argv[1]);
+//     LTKShapeRecognizer *pShapeReco = NULL;  // GLOBAL NOW !!!
+    ptrObj->createShapeRecognizer(strLogicalName, &pShapeReco);
+    if(pShapeReco == NULL)
+    {
+        cout << endl << "Error creating Shape Recognizer" << endl;
+        //cout << getError(ptrObj->getLastError()) << endl;
+
+
+        utilPtr->unloadSharedLib(hLipiEngine);
+        delete utilPtr;
+        return -1;
+    }
+
+    //You can also use project and profile name to create LipiEngine instance as follows...
+    //string strProjectName = "hindi_gestures";
+    //string strProfileName = "default";
+    //LTKShapeRecognizer *pReco = ptrObj->createShapeRecognizer(&strProjectName, &strProfileName);
+
+    //Load the model data into memory before starting the recognition...
+    iResult = pShapeReco->loadModelData();
+    if(iResult != SUCCESS)
+    {
+        cout << endl << iResult << ": Error loading Model data." << endl;
+      //cout << getError(pShapeReco->getLastError()) << endl;
+        ptrObj->deleteShapeRecognizer(&pShapeReco);
+
+        utilPtr->unloadSharedLib(hLipiEngine);
+        delete utilPtr;
+
+        return -1;
+    }
+
+
+    cout << endl << "Input Logical project name = " << strLogicalName << endl;
+//     cout << endl << "Input ink file for recognition = " << path << endl;
+
+
+    return 0;
+}
+
+
+
+
+//Declare variables to be used for recognition...
+LTKCaptureDevice captureDevice;
+LTKScreenContext screenContext;
+vector<int> shapeSubset;
+int numChoices = 2;
+float confThreshold = 0.0f;
+vector<LTKShapeRecoResult> results;
+// LTKTraceGroup inTraceGroup;
+
+
+int shaperectst_recog(LTKTraceGroup &inTraceGroup)
+{
+    int iResult;
+
+    
+    // You can directly read the UNIPEN ink file which has all the
+    // device context, screen context and ink information and pass it to
+    // recognize function. Or you can create your own LTKTrace and populate
+    // device and screen information as in commented code below.
+    // Read the ink to be recognized from the file...
+//     string path(argv[2]);
+//     LTKInkFileReader::readUnipenInkFile(path, inTraceGroup, captureDevice, screenContext);
+
+    // Set device context information to pass onto the recognizer
+    // Uncomment and pass proper values here...
+    //captureDevice.setSamplingRate(<float value>);
+    //captureDevice.setXDPI(<float value>);
+    //captureDevice.setYDPI(<float value>);
+    //captureDevice.setLatency(<float value>);
+    //captureDevice.setUniformSampling(<true or false>);
+
+    //  Set the device context, once before starting the recognition...
+    pShapeReco->setDeviceContext(captureDevice);
+
+    results.clear();
+    results.reserve(numChoices);
+//     results.reserve(1);
+
+    // Uncomment and edit the following lines to pass ink from the pen device
+    // to recognize function.
+    // The functions should copy the values from your local ink & screen
+    // structures into inTraceGroup and screenContext variables.
+    // CopyToTraceGroup(inTraceGroup, <your ink structure>);
+    // CopyScreenContext(screenContext, <your screen info>);
+
+    //now call the "recognize" method
+    iResult = pShapeReco->recognize(inTraceGroup, screenContext, shapeSubset, confThreshold, numChoices, results);
+    if(iResult != SUCCESS)
+    {
+        cout << iResult << ": Error while recognizing." << endl;
+        //cout << getError(pShapeReco->getLastError()) << endl;
+        ptrObj->deleteShapeRecognizer(&pShapeReco);
+
+        utilPtr->unloadSharedLib(hLipiEngine);
+
+        delete utilPtr;
+        return -1;
+    }
+
+    cout << endl << "Recognition Results\n\n";
+
+    //Display the recognized results...
+    for(int index =0; index < results.size(); ++index)
+    {
+        cout << "Choice[" << index << "] " << "Recognized Shapeid = " << results[index].getShapeId() << " Confidence = " << results[index].getConfidence() << endl;
+    }
+
+    cout << "Numero reconocido: " <<  results[0].getShapeId() << " !!!!\n";
+
+    return results[0].getConfidence();
+}
+
+int shaperectst_end()
+{
+    //Delete the shape recognizer object
+    ptrObj->deleteShapeRecognizer(&pShapeReco);
+
+    //unload the LipiEngine module from memory...
+    utilPtr->unloadSharedLib(hLipiEngine);
+
+    delete utilPtr;
+}
+
+
+
+/**********************************************************************************
+* NAME          : MapFunctions
+* DESCRIPTION   : This method fetches the address of the exported function of
+*                 lipiengine module
+* ARGUMENTS     :
+* RETURNS       : 0 on success, -1 on Failure.
+* NOTES         :
+* CHANGE HISTROY
+* Author            Date                Description of change
+*
+*************************************************************************************/
+int MapFunctions()
+{
+    createLTKLipiEngine = NULL;
+    void* functionHandle = NULL;
+
+    int iErrorCode = utilPtr->getFunctionAddress(hLipiEngine,
+                                             "createLTKLipiEngine",
+                                             &functionHandle);
+
+
+    createLTKLipiEngine = (FN_PTR_CREATELTKLIPIENGINE)functionHandle;
+
+    if(iErrorCode != SUCCESS)
+    {
+        cout << "Error mapping the createLTKLipiEngine function" << endl;
+        return -1;
+    }
+
+    return 0;
 }
